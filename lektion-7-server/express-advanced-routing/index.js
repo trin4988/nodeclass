eb623422e73045214af1a65cae9a493c5047666e
Heinz K <heka@techcollege.dk>
2019-01-31 08:41:18 +0100

    Test af express

---
+++ lektion-7-server/express-advanced-routing/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+//Sætter view engine til ejs
+app.set('view engine', 'ejs');
+
+//Korrigerer view mappe
+//app.set('views','lektion-7-server/express-advanced-routing/views');
+
+app.get("/sange", (req, res) => {
+    console.log(req.params);
+    res.send("Liste over sange");
+});
+
+app.get("/sange/:id", (req, res) => {
+    console.log(req.params);
+    res.json({
+        params: req.params
+    });
+});
+
+//Angiver port der skal lyttes på
+app.listen(4242, () => {
+    console.log("Express server kører...");
+});
